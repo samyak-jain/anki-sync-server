462490d5f40ebea9b396919388ae9608071e73e6
Vikash Kothary <kothary.vikash@gmail.com>
2020-07-30 22:38:53 +0100

    Remove editable install until setup.py is added

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -6,4 +6,4 @@ poetry lock
 poetry export --without-hashes -f requirements.txt > src/requirements.txt
 poetry export --dev --without-hashes -f requirements.txt > src/requirements-dev.txt
 
-echo "-e src/." >> src/requirements-dev.txt
+# echo "-e src/." >> src/requirements-dev.txt
